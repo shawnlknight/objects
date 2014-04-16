cf97a8c10005a5b1c7496af2afcfe2b665d313ba
Shawn Leberknight <shawnleberknight@Shawns-MacBook-Pro.local>
2014-04-15 21:31:38 -0400

    add objects & constructors

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,154 @@
 console.log('\'Allo \'Allo!');
+
+$(document).ready(function() {
+
+
+// object template 1
+var albums = [
+	{
+		title: "'The Question'",
+		artist: "Emery",
+		year: "2005"
+	},
+	{
+		title: "'Come Now Sleep'",
+		artist: "As Cities Burn",
+		year: "2007"
+	},
+	{
+		title: "'Beggars'",
+		artist: "Thrice",
+		year: "2009"
+	},
+	{
+		title: "'How to Start a Fire'",
+		artist: "Further Seems Forever",
+		year: "2003"
+	}
+];
+
+	var markup = "";
+
+	$.each(albums, function(key, value) {
+	
+		markup += "<h4>" + albums[key].title + "</h4><h5>" + albums[key].artist + "</h5><p>" + albums[key].year + "</p>";
+
+	});
+
+$(".albums").append(markup);
+
+
+
+
+// object template 2
+var records = [
+	{
+		movement: "Squat",
+		pr: "225 lbs"
+	},
+	{
+		movement: "Deadlift",
+		pr: "300 lbs"
+	},
+	{
+		movement: "Clean & Jerk",
+		pr: "185 lbs"
+	},
+	{
+		movement: "Muscle Ups",
+		pr: "5 in a row"
+	},
+	{
+		movement: "100 Burpees",
+		pr: "6:00 min"
+	}
+];
+
+	var markup = "";
+
+	$.each(records, function(key, value) {
+
+		markup += "<h3>" + records[key].movement + "</h3><p>" + records[key].pr + "</p>";
+
+	}); 
+
+$(".records").append(markup);
+
+});
+
+
+
+
+// object template 3
+var bass = {
+	type: "jazz",
+	brand: "fender",
+	stings: 4,
+	color: "black",
+
+	changeColor: function(newColor) {
+		this.color = newColor;
+		return this.color;
+	}
+};
+
+
+
+
+
+
+// Constructor 1
+function Car(make, model, color, needGas) {
+	this.make = make;
+	this.model = model;
+	this.color = color;
+	this.needGas = needGas;
+	this.fuel = function() {
+		this.needGas = false;
+	};
+}
+
+var bubba = new Car("honda", "civic", "silver", true);
+
+
+// Constructor 2
+function Friend(location, age, gender, hasBeard) {
+	this.location = location;
+	this.age = age;
+	this.gender = gender;
+	this.hasBeard = hasBeard;
+	this.shave = function() {
+		this.hasBeard = false;
+	};
+}
+
+var jonny = new Friend("colorado", 27, "male", true);
+var kevin = new Friend("california", 26, "male", true);
+var david = new Friend("south carolina", 28, "male", true);
+
+
+
+
+
+
+
+// Constructor 3 (for fun)
+function Coffee(roast, origin, brewMethod) {
+	this.roast = roast;
+	this.origin = origin;
+	this.brewMethod = brewMethod;
+	this.french = function() {
+		this.brewMethod = "french press";
+	};
+	this.spoon = function() {
+		this.brewMethod = "eat strait with a spoon!";
+	}
+}
+
+var house = new Coffee("medium", "south america", "pour over");
+var sumatra = new Coffee("dark", "asia", "drip");
+var special = new Coffee("extra-dark", "ethiopia", "pour over");
+
+
+
+
